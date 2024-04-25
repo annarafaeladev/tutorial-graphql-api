649fabb23f9fd6f508dca95689e3b803109a0ef8
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2024-04-25 15:32:24 -0300

    feat: first commit

---
+++ src/main/java/com/br/annasantana/tutorialgraphqlapi/TutorialGraphqlApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.annasantana.tutorialgraphqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialGraphqlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialGraphqlApiApplication.class, args);
+	}
+
+}
