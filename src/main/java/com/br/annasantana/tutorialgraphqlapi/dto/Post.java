4c0592da64ddfd4690a089a0d0cec959ed6df8e2
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2024-04-25 15:29:33 -0300

    feat: first commit

---
+++ src/main/java/com/br/annasantana/tutorialgraphqlapi/dto/Post.java
@@ -0,0 +1,3 @@
+package com.br.annasantana.tutorialgraphqlapi.dto;
+
+public record Post(String id, String content) {}
---
+++ src/test/java/com/br/annasantana/tutorialgraphqlapi/TutorialGraphqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.annasantana.tutorialgraphqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialGraphqlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
