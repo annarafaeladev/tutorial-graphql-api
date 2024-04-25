6a0f90098c54ffa937c17b755f4e9041be83dbed
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2024-04-25 16:41:48 -0300

    feat: implement CommentService

---
+++ src/main/java/com/br/annasantana/tutorialgraphqlapi/service/CommentService.java
@@ -0,0 +1,21 @@
+package com.br.annasantana.tutorialgraphqlapi.service;
+
+import com.br.annasantana.tutorialgraphqlapi.dto.Comment;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class CommentService {
+    Map<String, Comment> comments = new HashMap<>();
+
+    public Collection<Comment> createComment(String content, String postId) {
+        var newComment = new Comment(UUID.randomUUID().toString(), content, postId);
+        comments.put(newComment.id(), newComment);
+
+        return comments.values();
+    }
+}
